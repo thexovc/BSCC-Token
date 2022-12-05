28162f66442485787dd916e555cface9da7c2948
Osariemen Daniel Osazee <63324071+kyilax@users.noreply.github.com>
2022-12-05 04:58:59 +0100

    added footer and made some constants

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='text-gray-300 bottom-0 fixed right-0 p-4 px-8'>
+            <p>
+                12 gwei
+                <span className='pl-2'>16105747</span>
+            </p>
+        </div>
+    )
+}
+
+export default Footer
