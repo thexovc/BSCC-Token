9b83eba22a3e65493ec74898695b327e5fe80edd
Osariemen Daniel Osazee <63324071+kyilax@users.noreply.github.com>
2022-12-05 10:52:30 +0100

    changed the footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { BsTwitter } from 'react-icons/bs'
 
 const Footer = () => {
     return (
-        <div className='text-gray-300 w-full pt-10 p-4 px-8 bg-[#101422]'>
+        <div className='text-gray-300 w-full p-4 px-8 bg-[#101422]'>
             <a target="_blank" href="https://twitter.com/thexovc" className='bg-[#101422] flex justify-center items-center cursor-pointer md:text-2xl'>
                 <BsTwitter className='text-xl md:text-3xl text-blue-400' />
                 <span className='pl-2 text-orange-300'>Daniel Osariemen</span>
