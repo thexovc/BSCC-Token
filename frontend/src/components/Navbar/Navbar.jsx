6c4ca7bef5653c41767d2d07ff9358a399be73ae
Osariemen Daniel Osazee <63324071+kyilax@users.noreply.github.com>
2022-12-05 04:45:40 +0100

    added navbar

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react'
+import logo from '../../assets/svg/logo.svg'
+import { FaEthereum } from 'react-icons/fa'
+import bnb from '../../assets/images/bnb.png'
+import { BContext } from '../../context/BContext'
+
+const Navbar = () => {
+    const { connectWallet, currentAccount } = useContext(BContext)
+
+    return (
+        <nav className='md:w-full w-screen flex text-white p-4 justify-between'>
+            <div className="flex items-center md:w-4/12 ">
+                <ul className='flex justify-between w-full px-4 text-xl items-center'>
+                    <a>
+                        <li className='border-4 border-green-400 rounded-full'>
+                            <img src={bnb} className='w-[60px] h-[60px] border-4 border-white rounded-full ' alt="" />
+                        </li>
+                    </a>
+
+                </ul>
+            </div>
+
+            <div className='w-4/12 px-2 hidden md:flex'>
+                <input className='w-full p-4 text-xl bg-gray-700 bg-transparent rounded-lg border-gray-300 border-2' placeholder='Search Tokens and NFTs Collections ' />
+            </div>
+
+            <div className='md:w-3/12 text-sm  md:text-xl flex items-center gap-20 px-4'>
+
+                <p className='hidden md:flex md:items-center md:gap-1'>
+                    <span className='p-1 bg-[#637FEA]  rounded-full'>
+                        <FaEthereum />
+                    </span>
+                    <span>BNB</span>
+
+                </p>
+                <p onClick={connectWallet} className='px-3 py-3 gap-1 cursor-pointer bg-[#213159] flex items-center rounded-l-3xl md:justify-evenly text-[#4D83FB]  md:items-center rounded-r-3xl '>
+                    {currentAccount ? (
+                        <>
+                            {currentAccount.slice(0, 8)}...
+                        </>) : "Connect"}
+
+                </p>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
